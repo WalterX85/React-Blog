b87f7590cb31efeb048089e37b42643394f83ffc
WalterX85 <cedric.guyot2@gmail.com>
2021-10-01 10:45:29 +0200

    home and card

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react'
+import './Navbar.css'
+
+function Navbar () {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [largeur, setLargeur] = useState(window.innerWidth);
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect (() => {
+
+        const changeWidth = () => {
+            setLargeur(window.innerWidth)
+        }
+
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth);
+        }
+
+    }, [])
+
+    return (
+        <nav>
+            {(toggleMenu || largeur > 500) && (
+            <ul className="liste">
+                <li className="items">Accueil</li>
+                <li className="items">Ecrire</li>
+                <li className="items">Contact</li>
+            </ul>
+            )}
+            <button onClick={toggleNav} className="btn">Button</button>
+            
+        </nav>
+    )
+}
+
+export default Navbar 
---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import './Home.css'
+import Card from '../../Components/Card/Card'
+
+function Home() {
+
+  return (
+
+    <>
+      <h1 className="home-title">Tous les articles</h1>
+      <div className="container-cards">
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+        <Card>
+          <h2>Hello Card</h2>
+        </Card>
+      </div>
+    </>
+  )
+}
+
+export default Home
